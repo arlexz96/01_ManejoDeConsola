55773fbc382c0508c68916d8dd107321400fe38e
arlexz96 <arlex.z96@gmail.com>
2024-04-15 13:47:30 -0500

    se realizan ejercicios 1 y 2

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,12 @@
 // let phoneNumber = "3116200055"
 // let afirmative = true
 
-var userName = prompt("Enter Your UserName") /* antes del 2015 */ 
-let lastName = prompt("Enter Your LastName") /* se recomienda utilizar */
-const age = prompt("Enter Your Age") /* se recomienda utilizar */
-const email = prompt("Enter Your Email")
-let address = prompt("Enter Your Address")
-let phoneNumber = prompt("Enter Your PhoneNumber")
+// var userName = prompt("Enter Your UserName") /* antes del 2015 */ 
+// let lastName = prompt("Enter Your LastName") /* se recomienda utilizar */
+// const age = prompt("Enter Your Age") /* se recomienda utilizar */
+// const email = prompt("Enter Your Email")
+// let address = prompt("Enter Your Address")
+// let phoneNumber = prompt("Enter Your PhoneNumber")
 
 /* console.log(userName)
 console.error(lastName)
@@ -22,11 +22,11 @@ console.log(phoneNumber)
 console.assert(afirmative)
  */
 
-console.log("Nombre: "+userName+" "+lastName+" - Edad: "+age+" - Email: "+email+" - Direccion: "+address+" - Numero De Telefono: "+phoneNumber)
+// console.log("Nombre: "+userName+" "+lastName+" - Edad: "+age+" - Email: "+email+" - Direccion: "+address+" - Numero De Telefono: "+phoneNumber)
 
-console.warn("Nombre:",userName,lastName,"- Edad:",age,"- Email:",email,"- Direccion:",address,"- Numero De Telefono:",phoneNumber)
+// console.warn("Nombre:",userName,lastName,"- Edad:",age,"- Email:",email,"- Direccion:",address,"- Numero De Telefono:",phoneNumber)
 
-console.error(`Nombre: ${userName} ${lastName} - Edad: ${age} - Email: ${email} - Direccion: ${address} - Numero De Telefono: ${phoneNumber}`)
+// console.error(`Nombre: ${userName} ${lastName} - Edad: ${age} - Email: ${email} - Direccion: ${address} - Numero De Telefono: ${phoneNumber}`)
 
 /* //GRUPOS
 console.group("Data Basic Information")
---
+++ src/js/main2.js
@@ -0,0 +1,46 @@
+// //Ejercicio 1
+
+// let n1 = prompt("Ingrese el primer numero");
+// let n2 = prompt("Ingrese el segundo numero");
+
+// console.info(`La suma de los numeros es: ${parseInt(n1) + parseInt(n2)}`);
+// console.log(`La resta de los numeros es: ${parseInt(n1) - parseInt(n2)}`);
+// console.warn(
+//   `La multiplicacion de los numeros es: ${parseInt(n1) * parseInt(n2)}`
+// );
+// if (n2 < 1)
+//   console.error(
+//     "El numero del denominador es = 0 por lo que no se puede realizar la division"
+//   );
+// else
+//   console.error(
+//     `la division de los numeros es: ${parseFloat(n1) / parseFloat(n2)}`
+//   );
+
+// //EJERCICIO 2
+// fruits = [];
+// citrusFruits = [];
+// noCitrusFruits = [];
+// let opcion = 1;
+
+// while (opcion != 0){
+//     let fruit = prompt("Ingrese una fruta");
+//     fruits.push(fruit);
+//     let citrus = parseInt(prompt("¿La fruta es citrica?    1. SI ---- 0. NO"));
+//     if (citrus == 1)
+//         citrusFruits.push(fruit);
+//     else 
+//         noCitrusFruits.push(fruit);
+//     opcion = prompt("¿Desea Ingresar otra fruta?    1. SI ---- 0. NO")
+// }
+
+// console.log(fruits);
+// console.table(citrusFruits);
+// console.table(noCitrusFruits);
+
+
+//EJERCICIO 3
+
+let nota = 0
+notas = []
+let nroAsignaturas = 
